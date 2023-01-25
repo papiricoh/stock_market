bb1637235aaa358bd72ffb2a3f72eb1daf6a1448
papiricoh <slendeman0200@gmail.com>
2023-01-25 14:06:33 +0100

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import StockChart from './components/StockChart.vue'
+</script>
+
+<template>
+  <main>
+    <div>
+      <StockChart></StockChart>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/StockChart.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  data() {
+    return {
+      options: {
+        chart: {
+          type: 'area',
+          id: 'stock_chart',
+          toolbar: {
+            autoSelected: 'pan',
+            show: false
+          }
+        },
+        dataLabels: {
+          enabled: false
+        },
+        xaxis: {
+          categories: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        }
+      },
+      series: [{
+        name: 'series-1',
+        data: [12000, 12200, 13000, 11200, 10000, 9000, 8200, 10000, 10020]
+      }]
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <apexchart width="500" type="area" :options="options" :series="series"></apexchart>
+  </div>
+</template>
